beae7563329df10d91d3c17701f3596e4c70c1b1
Bryan Cranston <>
2023-05-23 22:58:36 +0300

    feat: hide hints if finish button pressed

--- src/inject.ts
+++ src/inject.ts
@@ -5,10 +5,16 @@ declare global {
     interface Window {
         contenidorellenado: string; // [["answer1", 0, ...], ["answer2", 1, ...]]
         comprobarRespuestas(numero: 0 | 1): void; // solve worksheet function
+        heTerminado(): void; // default #capafinish button function
     }
 }
 
 document.body.oncontextmenu = null;
+const finishButton = document.querySelector("#capafinish") as HTMLDivElement;
+finishButton.onclick = (): void => {
+    window.heTerminado();
+    hideHintText();
+};
 
 function hideHintText(): void {
     const classNames: string[] = ["joindiv", "dragablediv", "dropdiv"];
